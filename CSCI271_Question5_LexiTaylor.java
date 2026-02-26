70401b8612c58f1476c98eb1f09525597cdce286
taylora44 <taylora44@mailbox.winthrop.edu>
2026-02-25 22:41:28 -0500

    Update CSCI271_Question5_LexiTaylor.java

--- CSCI271_Question5_LexiTaylor.java
+++ CSCI271_Question5_LexiTaylor.java
@@ -1,6 +1,49 @@
+/*************************************************************************
+* Assignment 3 for CSCI 271-001 Spring 2026
+*
+* Author: Lexi Taylor
+* OS: Windows
+* Compiler: javac
+* Date: February 25, 2026
+*
+* Purpose
+* This program is one part of a six part project. This specific question
+* reads string s and displays it backwards
+*
+*************************************************************************/
+/*******************************************************************
+* I declare and confirm the following:
+* - I have not discussed this program code with anyone other than my
+* instructor or the teaching assistants assigned to this course.
+* - I have not used programming code obtained from someone else,
+* or any unauthorised sources, including the Internet, either
+* modified or unmodified.
+* - If any source code or documentation used in my program was
+* obtained from other sources, like a text book or course notes,
+* I have clearly indicated that with a proper citation in the
+* comments of my program.
+* - I have not designed this program in such a way as to defeat or
+* interfere with the normal operation of the supplied grading code.
+*
+* <Lexi Taylor>
+********************************************************************/
 import java.util.Scanner;
 public class CSCI271_Question5_LexiTaylor {
 
+/*****************************reverseRecursive****************************
+* Description: reads string s and displays it backwards
+*
+* Parameters: s - the string
+*
+* Pre: s can be null or any string
+*
+* Post: The function is not modifying s
+*
+* Returns: a string that is the reverse of s
+*
+* Called by: main
+* Calls: reverseRecursive, s.substring
+************************************************************************/
     public static String reverseRecursive(String s) {
         if (s == null || s.length() <= 1) { //this is the base case, if it is empty or a single character
             return s;
