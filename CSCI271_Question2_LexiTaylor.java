a3a3424e417ef7bc50f86ee5f702c84cf0d287b6
taylora44 <taylora44@mailbox.winthrop.edu>
2026-02-25 21:22:50 -0500

    Enhance documentation in CSCI271_Question2_LexiTaylor.java
    Added a detailed header comment and method documentation for countChar.

--- CSCI271_Question2_LexiTaylor.java
+++ CSCI271_Question2_LexiTaylor.java
@@ -1,5 +1,51 @@
+/*************************************************************************
+* Assignment 3 for CSCI 271-001 Spring 2026
+*
+* Author: Lexi Taylor
+* OS: Windows
+* Compiler: javac
+* Date: February 25, 2026
+*
+* Purpose
+* This program is one part of a six part project. This specific question
+* reads a string and single character c and displays the number of times
+* c occurs in s
+*
+*************************************************************************/
+/*******************************************************************
+* I declare and confirm the following:
+* - I have not discussed this program code with anyone other than my
+* instructor or the teaching assistants assigned to this course.
+* - I have not used programming code obtained from someone else,
+* or any unauthorised sources, including the Internet, either
+* modified or unmodified.
+* - If any source code or documentation used in my program was
+* obtained from other sources, like a text book or course notes,
+* I have clearly indicated that with a proper citation in the
+* comments of my program.
+* - I have not designed this program in such a way as to defeat or
+* interfere with the normal operation of the supplied grading code.
+*
+* <Lexi Taylor>
+********************************************************************/
 import java.util.Scanner;
 public class CSCI271_Question2_LexiTaylor {
+
+/*****************************CountChar****************************
+* Description: Recursively counts how many times c occurs in s
+*
+* Parameters: s - the string, c - the character we are counting
+*
+* Pre: s can be null or any string
+*
+* Post: The function is not modifying s
+*
+* Returns: The number of times c appears in s as an int
+*
+* Called by: main
+* Calls: countChar, s.substring
+************************************************************************/
+
     public static int countChar(String s, char c) {
         if (s == null || s.length() == 0) { //base case where null is a empty string
             return 0;
