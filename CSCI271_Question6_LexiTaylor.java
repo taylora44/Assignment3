42c7de0a2ea39e5345b30dee67b0fdb62bc63dcb
taylora44 <taylora44@mailbox.winthrop.edu>
2026-02-25 22:46:29 -0500

    Implement sumEven method and update documentation
    Added documentation comments and method sumEven to calculate the sum of even numbers in an array.

--- CSCI271_Question6_LexiTaylor.java
+++ CSCI271_Question6_LexiTaylor.java
@@ -1,6 +1,50 @@
+/*************************************************************************
+* Assignment 3 for CSCI 271-001 Spring 2026
+*
+* Author: Lexi Taylor
+* OS: Windows
+* Compiler: javac
+* Date: February 25, 2026
+*
+* Purpose
+* This program is one part of a six part project. This specific question
+* reads a a list of integers from the keyboard and stores them in an array
+* then the program will sum all numbers in the array together
+*
+*************************************************************************/
+/*******************************************************************
+* I declare and confirm the following:
+* - I have not discussed this program code with anyone other than my
+* instructor or the teaching assistants assigned to this course.
+* - I have not used programming code obtained from someone else,
+* or any unauthorised sources, including the Internet, either
+* modified or unmodified.
+* - If any source code or documentation used in my program was
+* obtained from other sources, like a text book or course notes,
+* I have clearly indicated that with a proper citation in the
+* comments of my program.
+* - I have not designed this program in such a way as to defeat or
+* interfere with the normal operation of the supplied grading code.
+*
+* <Lexi Taylor>
+********************************************************************/
 import java.util.Scanner;
 public class CSCI271_Question6_LexiTaylor {
 
+/*****************************SumDigit****************************
+* Description: calculates the sum of numbers in an array
+*
+* Parameters: n - non-negative number
+*
+* Pre: n >= 0
+*
+* Post: The function does not modify n
+*
+* Returns: The sum of all products in the array
+*
+* Called by: main
+* Calls: sumDigitPairs
+************************************************************************/
     public static int sumEven(int[] A, int n) {
         if (n == 0) {
             return 0;
